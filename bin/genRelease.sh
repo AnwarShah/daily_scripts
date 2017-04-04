a39b5e94ae2584309bc85398a34aaeab01194c03
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-04-04 14:21:34 +0600

    adds genRelease script

---
+++ bin/genRelease.sh
@@ -0,0 +1,9 @@
+cat > Release <<EOF
+Archive: Xenial
+Origin: Ubuntu
+Suite: ubuntu-local
+Label: Local Ubuntu Repository
+Architecture: amd64 i386
+EOF
+apt-ftparchive release . >> Release
+gpg -abs -o Release.gpg Release
