7c0293804ba22e5488cecbee0cd16d134c3a2ef2
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-04-14 21:25:30 +0600

    update backup_debs to create Packages index as well

--- bin/backup_debs.sh
+++ bin/backup_debs.sh
@@ -1,10 +1,10 @@
-backup_dir="/media/anwar/Data/Software/Linux/Debian_Packages/debs_new/"
+backup_dir="/media/anwar/Data/Software/Linux/Debian_Packages/xenial_new/"
 archive_dir="/var/cache/apt/archives/"
 
 echo "copying newly updated debs to ... $backup_dir"
-cp -u -v $archive_dir/*.deb $backup_dir
+sudo mv -u -v $archive_dir/*.deb $backup_dir
 
-echo "deleting deb files in archive..."
-sudo rm $archive_dir/*.deb
-
+# Now update the package index file
+cd $backup_deb/
+apt-ftparchive packages . > Packages
 
