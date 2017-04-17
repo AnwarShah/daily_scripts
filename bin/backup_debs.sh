efdf15459fce660cd5c5d7750715d9dbe98f3338
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-04-17 16:27:37 +0600

    use cautious cp instead of mv in backup_debs

--- bin/backup_debs.sh
+++ bin/backup_debs.sh
@@ -2,7 +2,7 @@ backup_dir="/media/anwar/Data/Software/Linux/Debian_Packages/xenial_new/"
 archive_dir="/var/cache/apt/archives/"
 
 echo "copying newly updated debs to ... $backup_dir"
-sudo mv -u -v $archive_dir/*.deb $backup_dir
+sudo cp -u -v $archive_dir/*.deb $backup_dir
 
 # Now update the package index file
 cd $backup_deb/
