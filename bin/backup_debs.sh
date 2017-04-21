a18981d437ccf8692a210a78afe4ef6f38dc5f52
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-04-21 10:57:47 +0600

    don't use sudo in copying debs

--- bin/backup_debs.sh
+++ bin/backup_debs.sh
@@ -1,10 +1,10 @@
 backup_dir="/media/anwar/Data/Software/Linux/Debian_Packages/xenial_new/"
-archive_dir="/var/cache/apt/archives/"
+archive_dir="/var/cache/apt/archives"
 
 echo "copying newly updated debs to ... $backup_dir"
-sudo cp -u -v $archive_dir/*.deb $backup_dir
+cp -u -v $archive_dir/*.deb $backup_dir
 
 # Now update the package index file
 cd $backup_deb/
-apt-ftparchive packages . > Packages
+genPackages.sh && genRelease.sh
 
