0da362ddce62515506c42f831b2934ffec71c509
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-05-22 14:19:46 +0600

    generate gem index after backup

--- bin/backup_gems.sh
+++ bin/backup_gems.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-find $HOME/.rvm -follow -iname '*.gem' -type f -exec cp -u -v {} /media/anwar/Data/Software/RubyGems/gems/ \;
+gems_backup_dir="/media/anwar/Data/Software/RubyGems"
+find $HOME/.rvm -follow -iname '*.gem' -type f -exec cp -u -v {} $gems_backup_dir/gems/ \;
+
+# generate index
+cd $gems_backup_dir
+gem generate_index
+cd -
