36391b499534c359d7c44794d3dce6c6953f897a
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-06-03 12:14:15 +0600

    updated backup_debs to use release name

--- bin/backup_debs.sh
+++ bin/backup_debs.sh
@@ -1,5 +1,11 @@
-backup_dir="/media/anwar/Data/Software/Linux/Debian_Packages/xenial_new/"
+release_name=$(lsb_release -cs)
+backup_dir="/media/anwar/Data/Software/Linux/Debian_Packages/"$release_name"_new/"
 archive_dir="/var/cache/apt/archives"
+
+if [ ! -f $backup_dir ];then
+    echo "Creating ..." $backup_dir
+    mkdir $backup_dir
+fi
 
 echo "copying newly updated debs to ... $backup_dir"
 cp -u -v $archive_dir/*.deb $backup_dir
