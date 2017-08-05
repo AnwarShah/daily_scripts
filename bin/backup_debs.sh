341c305d53a13305a16609114b942b638855d3f1
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-08-06 02:30:55 +0600

    don't generate package file if backup dir doesn't exist

--- bin/backup_debs.sh
+++ bin/backup_debs.sh
@@ -1,5 +1,5 @@
 release_name=$(lsb_release -cs)
-backup_dir="/media/anwar/Data/Software/Linux/Debian_Packages/"$release_name"_new/"
+backup_dir="/mnt/Data/Software/Linux/Debian_Packages/"$release_name"_new/"
 archive_dir="/var/cache/apt/archives"
 
 if [ ! -f $backup_dir ];then
@@ -11,6 +11,7 @@ echo "copying newly updated debs to ... $backup_dir"
 cp -u -v $archive_dir/*.deb $backup_dir
 
 # Now update the package index file
-cd $backup_dir/
-genPackages.sh && genRelease.sh
+if cd $backup_dir/; then
+  genPackages.sh && genRelease.sh
+fi
 
