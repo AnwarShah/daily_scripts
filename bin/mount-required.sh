534b1db5a9f004a386e2ae1e076e0ba16dfa545e
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-09-06 20:07:47 +0600

    fixes the problem when loop-devices aren't mounted automatically

--- bin/mount-required.sh
+++ bin/mount-required.sh
@@ -1,9 +1,18 @@
-if [ -z $(mount | grep -o "/mnt/shared/Hedera.img" ) ]; then
-  udisksctl loop-setup --file /mnt/shared/Hedera.img
+# first setup loop devices and read the loop device names
+udisksctl loop-setup --file /mnt/shared/Papirus.iso
+udisksctl loop-setup --file /mnt/shared/Hedera.img
+
+# Get the associated loop devices
+hedera_loop=$(losetup | grep "/mnt/shared/Hedera.img" | cut -d ' ' -f 1 | head -n 1)
+papirus_loop=$(losetup | grep "/mnt/shared/Papirus.iso" | cut -d ' ' -f 1 | head -n 1)
+
+# Now if the loops aren't mounted automatically, mount them
+if [ -z $(mount | grep -o $papirus_loop) ]; then
+  udisksctl mount --block-device $papirus_loop
 fi
 
-if [ -z $(mount | grep -o "/mnt/shared/Papirus.iso" ) ];then
-  udisksctl loop-setup --file /mnt/shared/Papirus.iso
+if [ -z $(mount | grep -o $hedera_loop ) ];then
+  udisksctl mount --block-device $hedera_loop
 fi
 
 if [ -z $(mount | grep -o Zesty) ]; then
