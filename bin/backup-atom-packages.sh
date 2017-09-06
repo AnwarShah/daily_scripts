2c607317104257a3da22cd3db9a449d7eb64b3b5
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-09-06 14:17:13 +0600

    adds atom-packages-backup script

---
+++ bin/backup-atom-packages.sh
@@ -0,0 +1,17 @@
+# Go to /mnt/home first
+cd /mnt/home
+
+PKG_DIR="$HOME/.atom/packages"
+BACKUP_FILE_BASENAME="atom-packages-$(date +%d%m%Y)" #Day-Month-Year
+
+if [ ! -d $PKG_DIR ]; then
+  echo "Cannot find the directory $PKG_DIR. Exiting..."
+  return -1
+else
+  echo "Starting backup of $PKG_DIR at $(pwd)"
+  tar -cpjf $BACKUP_FILE_BASENAME.tar.bz2 --one-file-system $PKG_DIR
+  echo "Backup finished successfully."
+fi
+
+# Go back to previous dir
+cd -
