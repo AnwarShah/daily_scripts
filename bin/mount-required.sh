24cbb9189508e927adf57653588c4c8cd5e57863
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-09-07 19:17:08 +0600

    don't setup loop-setup if already exists

--- bin/mount-required.sh
+++ bin/mount-required.sh
@@ -1,10 +1,15 @@
-# first setup loop devices and read the loop device names
-udisksctl loop-setup --file /mnt/shared/Papirus.iso
-udisksctl loop-setup --file /mnt/shared/Hedera.img
-
-# Get the associated loop devices
+# Get the associated loop devices if already looped
 hedera_loop=$(losetup | grep "/mnt/shared/Hedera.img" | cut -d ' ' -f 1 | head -n 1)
 papirus_loop=$(losetup | grep "/mnt/shared/Papirus.iso" | cut -d ' ' -f 1 | head -n 1)
+
+# associate with loop devices if not already looped
+if [ -z $hedera_loop ]; then
+  hedera_loop=$(udisksctl loop-setup --file /mnt/shared/Hedera.img | grep -o -e "/dev/loop[0-9]*")
+fi
+
+if [ -z $papirus_loop ]; then
+  papirus_loop=$(udisksctl loop-setup --file /mnt/shared/Papirus.iso | grep -o -e "/dev/loop[0-9]*")
+fi
 
 # Now if the loops aren't mounted automatically, mount them
 if [ -z $(mount | grep -o $papirus_loop) ]; then
