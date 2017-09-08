c9e695680fb994cf4216f857033d36c7392364ce
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-09-09 00:35:37 +0600

    updates backup_gems script to check for backup dir first

--- bin/backup_gems.sh
+++ bin/backup_gems.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-gems_backup_dir="/media/anwar/Data/Software/RubyGems"
-find $HOME/.rvm -follow -iname '*.gem' -type f -exec cp -u -v {} $gems_backup_dir/gems/ \;
+gems_backup_dir="/mnt/Data/Software/RubyGems/"
+
+if [ -d $gems_backup_dir ]; then
+  find $HOME/.rvm -follow -iname '*.gem' -type f -exec cp -u -v {} $gems_backup_dir/gems/ \;
+else
+  echo "Backup directory doesn't exist at $gems_backup_dir"
+  exit -1
+fi
 
 # generate index
 cd $gems_backup_dir
