b51cdffe50131013ce9966cff45b72c66669e9bb
Mohammad Anwar Shah <Anwar Shah>
2018-10-06 19:44:06 +0600

    use udevil instead of squashfuse to mount sfs images

--- bin/mount-deb-sfs.sh
+++ bin/mount-deb-sfs.sh
@@ -1,12 +1,7 @@
 # mount the squashfs images of deb repos
 sqfsdir=/media/anwar/Software/Linux/Deb_Binaries
 
-#squashfuse -o allow_other $sqfsdir/xenial.squashfs  /mnt/xenial
-#squashfuse -o allow_other $sqfsdir/xenial2.squashfs /mnt/xenial2
-#squashfuse -o allow_other $sqfsdir/artful.squashfs  /mnt/artful
-#squashfuse -o allow_other $sqfsdir/zesty.squashfs   /mnt/zesty
-
 for img in {xenial,xenial2,zesty,artful}
 do
-  udevil mount $sqfsdir/$img.squashfs
+  udevil mount $sqfsdir/$img.squashfs $sqfsdir/$img/
 done
