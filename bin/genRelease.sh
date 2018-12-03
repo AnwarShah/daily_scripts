2b15de8657e916d9f2096c88954904c69151aa82
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2018-12-03 21:33:34 +0600

    updates genRelease to read release name input from user

--- bin/genRelease.sh
+++ bin/genRelease.sh
@@ -1,9 +1,14 @@
+echo -n "What will be the release name? Type and [Enter]: "
+read name
+title_name=${name[*]^}
+
 cat > Release <<EOF
-Archive: Xenial
-Origin: Ubuntu
-Suite: ubuntu-local
-Label: Local Ubuntu Repository
+Archive: $title_name
+Origin: Local
+Suite: $name-local
+Label: Local Ubuntu $title_name Repository
 Architecture: amd64 i386
 EOF
 apt-ftparchive --sha1=no --md5=no release . >> Release
 gpg --yes -abs -o Release.gpg Release
+
